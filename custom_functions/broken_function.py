a6b4fd61578c6d8c6b4d058520acde0c5bbdb8e7
cgunit6@gmail.com <cgunit6@gmail.com>
2026-05-13 13:55:42 +0000

    backing up work

---
+++ custom_functions/broken_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Broken Function"""
+
+
+
+def report_ghost_sighting(ghost_name):
+    print(f"{ghost_name} has been sighted! Who you gonna call?")
+
+report_ghost_sighting("Slimer")
+
---
+++ fix_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Transform the string
+title = "star trek"
+print(title.capitalize())
+
+
+# Define the string
+quote = "Live Long And Prosper"
+
+# Chain methods to lower the string, split it, and join with "-"
+result = "-".join(quote.lower().split())
+result = "-".join(quote.lower().split())
+print(result)
