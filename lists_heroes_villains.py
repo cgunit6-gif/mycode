f41b21189113ff84c459292dd95f49fb9d57fd4b
cgunit6@gmail.com <cgunit6@gmail.com>
2026-05-13 16:38:09 +0000

    backing up my code

---
+++ lists_heroes_villains.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Original lists
+marvel_heroes = ["Iron Man", "Captain America", "Thor", "Black Widow", "Hulk"]
+marvel_villains = ["Thanos", "Loki", "Ultron", "Hela", "Red Skull"]
+dc_heroes = ["Superman", "Batman", "Wonder Woman", "Flash", "Aquaman"]
+dc_villains = ["Joker", "Lex Luthor", "Darkseid", "Cheetah", "Reverse Flash"]
+
+# Empty list to hold combined data
+full_list = []
+
+# Combine the heroes and villains
+combined_heroes = []
+combined_heroes.extend(marvel_heroes)
+combined_heroes.extend(dc_heroes)
+
+combined_villains = []
+combined_villains.extend(marvel_villains)
+combined_villains.extend(dc_villains)
+
+# Append combined lists to the full list
+full_list.append(combined_heroes)
+full_list.append(combined_villains)
+full_list[0].append("Curtis")
+full_list[1].append("Melissa")
+
+# Print statements
+print(full_list)
+print("Just the heroes:", full_list[0])
+print("Just the villains:", full_list[1])
+print("Second character from the first list:", full_list[0][1])  # "Captain America"
+print("Last character from the last list:", full_list[1][2])  # "Ultron"
+
